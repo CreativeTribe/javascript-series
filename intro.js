855fcdda8a14224789cb477b030434290036700f
HassanBahati <mukisabahati@gmail.com>
2021-07-29 10:30:14 +0300

    introduction to javascript

---
+++ intro.js
@@ -0,0 +1,47 @@
+/* outputting 88
+console.log(88)
+*/
+//this is a comment 
+
+
+//literal and variables
+console.log(88)
+
+x = 66
+console.log(x)
+
+// operators +,-, *, /
+y = 2-4
+console.log(y)
+
+//keywords this, await, try , catch, 
+//let, const , var
+//cosnt -cannot be reaassigned
+//let/var -can reassigned
+
+const today = 2+5
+
+console.log(today)
+
+//data types
+//numbers
+//boleans
+// strings 
+//arrays
+//objects
+//classes
+let creative = "today is thursday";  //string
+let tribe = 66; //number
+let joe = true;  //boolean
+let _myArray = [4,5]
+
+console.log(typeof(_myArray))
+
+
+//
+let morning = 5
+let yes = 5
+console.log(morning + 7 + "was a great day")
+
+console.log(morning === yes) //cehcking datatype and value 
+console.log(morning == yes) //checking value

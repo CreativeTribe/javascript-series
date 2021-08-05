31bbd090e03e8b4dbd9337e346a4b463d96cddf3
HassanBahati <mukisabahati@gmail.com>
2021-08-05 10:31:43 +0300

    inbuiltfunctions

---
+++ inbuiltfunctions.js
@@ -0,0 +1,32 @@
+let name;
+let age;
+
+age = 20;
+name = "creativeMeetUps";
+
+let id = "My name is " + name + " and i am "+ age;
+
+
+console.log(id)
+
+let identity = `My name is ${name} and i am ${age}`
+
+console.log(identity)
+
+let length_of_identity = name.length
+
+console.log(length_of_identity)
+
+
+let name_in_lowercase = "creativetribe"
+let name_in_uppercase = "CREATIVETRIBE"
+
+console.log(name_in_lowercase.toUpperCase())
+console.log(name_in_uppercase.toLowerCase())
+
+
+let words = 'technology, mango'
+console.log(words.split(''))
+console.log(words.split(', '))
+
+console.log(words.bold())
